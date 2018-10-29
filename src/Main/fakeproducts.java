3cb9f69fddb7ecbd04e5c01a5179411aec992b60
ghermidont <mihai_olaru@ymail.com>
2018-10-29 18:36:44 +0200

    added infinite loop

--- src/Main/fakeproducts.java
+++ src/Main/fakeproducts.java
@@ -1,18 +1,34 @@
 package Main;
 import entities.products;
+import java.util.Scanner;
+
 
 public class fakeproducts {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        //HW while() will launch the menu after each choice
-        //MENU 1. create a new product
-        //2. Save product
-        //3. load product
-        //4. exit
+        for(;;) {
 
-        products p = new products();
-        System.out.println(p);
-        p.savetoFile("C:\\Users\\Mihail Olaru\\Desktop\\Javatext.txt");
+            System.out.println("Please chose the action you want to perform: \n1. Create a new product\n" +
+                    "2. Save the product\n3.Load the product\n4.Exit");
+            int choice = in.nextInt();
+            switch (choice){
+                case 1:{
+                    break;
+                }case 2: {
+                    break;
+                }case 3:{
+                    break;
+                }case 4:{
+                    System.out.println("GOOD BYE!!");
+                }
+            }
+            if(choice==4)break;
+        }
+
+      //products p = new products();
+   //     System.out.println(p);
+    //    p.savetoFile("Javatext.txt");
 
     }
 
