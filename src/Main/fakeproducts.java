1af47503db44d8d67c44a294799adb18d15a3b8e
ghermidont <mihai_olaru@ymail.com>
2018-10-30 16:42:01 +0200

    without data type checker..

--- src/Main/fakeproducts.java
+++ src/Main/fakeproducts.java
@@ -1,35 +1,14 @@
 package Main;
-import entities.products;
-import java.util.Scanner;
 
+import entities.products;
 
 public class fakeproducts {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
 
-        for(;;) {
 
-            System.out.println("Please chose the action you want to perform: \n1. Create a new product\n" +
-                    "2. Save the product\n3.Load the product\n4.Exit");
-            int choice = in.nextInt();
-            switch (choice){
-                case 1:{
-                    break;
-                }case 2: {
-                    break;
-                }case 3:{
-                    break;
-                }case 4:{
-                    System.out.println("GOOD BYE!!");
-                }
-            }
-            if(choice==4)break;
-        }
-
-      //products p = new products();
-   //     System.out.println(p);
-    //    p.savetoFile("Javatext.txt");
-
+    public static void main( String[] args ) {
+        products prod = new products();
+        prod.start();
     }
+
 
 }
--- src/entities/products.java
+++ src/entities/products.java
@@ -1,68 +1,153 @@
 package entities;
-import com.github.javafaker.Faker;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import com.github.javafaker.Faker;
+import java.io.*;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-
 public class products {
-    String category;
-    String name;
-    float price;
-    String manufacturer;
-    Date expires;
-    // generate with Faker
 
-    public products(){
-        Faker f = new Faker(new Locale("ru"));
-        this.category = f.lorem().word();
-        this.name = f.commerce().productName();
-        this.price = Float.valueOf(f.commerce().price(100, 1000));
-        this.manufacturer = f.chuckNorris().fact();
-        this.expires = f.date().future(1000, TimeUnit.DAYS);
-    }
+    private String category;
+    private String name;
+    private float price;
+    private String manufacturer;
+    private Date expires;
+    private int ordNumber = 0;
 
-    public products( String category, String name, float price, String manufacturer, Date expires){
-        this.category = category;
-        this.name = name;
-        this.price = price;
-        this.manufacturer = manufacturer;
-        this.expires = expires;
+    private void request_products(){
+
+        Faker f = new Faker( new Locale("en" ) );
+        //products INPUT
+
+        Scanner prodIN = new Scanner( System.in );
+
+            System.out.println("*******************************************************************\n* The program has no input data type checker.                     *\n* Thus, please input the data in the format indicated in the \"()\" *\n*******************************************************************\n" );
+            System.out.println("Please input the category of the new product (String):");
+            this.category = prodIN.nextLine();
+
+            System.out.println("\nPlease input the name of the product (String):");
+            this.name = prodIN.nextLine();
+
+            System.out.println("\nPlease input the price of the product (float data type format):");
+            this.price = prodIN.nextFloat();
+
+            System.out.println("\nPlease input the manufacturer of the product (String):");
+            this.manufacturer = prodIN.nextLine();
+
+            System.out.println("\n");
+            this.expires = f.date().future(1000, TimeUnit.DAYS);
+
     }
 
     @Override
     public String toString() {
-        return "products{\n" +
-                "category='" + category + '\'' + "\n" +
-                ", name='" + name + '\'' + "\n" +
-                ", price=" + price + "\n" +
-                ", manufacturer='" + manufacturer + '\'' + "\n" +
-                ", expires=" + expires +
-                '}';
+           return "\n" + "Product nr: " + ordNumber + "\n" +
+                "Category: '" + category + '\'' + "\n" +
+                "Name: '" + name + '\'' + "\n" +
+                "Price: " + price + " $\n" +
+                "Manufacturer: '" + manufacturer + '\'' + "\n" +
+                "Expiry date: " + expires +
+                "\n#################################\n";
+
     }
-//HW create another constructor that receives the info from the created file
-    // public Product(String file_name){
-    //
-    //       1. open file an -> intodice this.category = in.next() etc etc all the detail.
-    //  }
 
-    /*Scanner in =new Scanner( new File("myfolder/data.txt"));
-      String name = in.next();
-      Sysout(name);*/
+    private void readFromFile( String file_name ){
 
-    public void savetoFile(String file_name){
+       String line;
+
+       try {
+           BufferedReader in = new BufferedReader( new FileReader( file_name ) );
+
+           if ( in.readLine() == null ) {
+               System.out.println("\n!!! There are NO products in the DB... !!!\n");
+               start();
+           }else {
+              while( ( line = in.readLine() ) != null ){ System.out.println( line );}
+           }
+
+          in.close();
+
+       } catch ( IOException e ){
+          e.printStackTrace();
+       }
+
+    }
+
+    private void writeToDB( String file_name ){
 
             try {
-                FileWriter fw = new FileWriter(new File(file_name));
-                fw.write(this.toString());
+
+                FileWriter fw = new FileWriter( file_name, true );
+                fw.write( this.toString() );
                 fw.close();
-            } catch (IOException e) {
-                System.err.println("CANNOT SAVE");
+
+            } catch ( IOException e ) {
+
+                System.err.println( "CANNOT SAVE" );
+
             }
 
     }
+
+    private void deleteAllProducts( String file_name ){
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter( file_name );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fw.write( "" );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        Scanner in = new Scanner(System.in);
+        for (;;) {
+
+            System.out.println("\nPlease chose the action you want to perform: \n1. View existing products \n" +
+                    "2. Create a new product\n3. Delete all the products\n4. EXIT");
+
+            int choice = in.nextInt();
+
+            switch (choice) {
+
+                case 1: {
+                    readFromFile("Javatext.txt");
+                    break;
+                }
+                case 2: {
+                    request_products();
+                    ++ordNumber;
+                    writeToDB("Javatext.txt");
+                    break;
+                }
+                case 3: {
+                    deleteAllProducts("Javatext.txt");
+                    System.out.println("\nAll the products have been deleted from the DB.");
+                    break;
+                }
+                case 4: {
+                    System.out.println("GOOD BYE!!");
+                }
+                default:{
+                    System.out.println("Please input a valid option.");
+                }
+
+            }
+
+           if (choice == 4) break;
+
+        }
+    }
+
 }
